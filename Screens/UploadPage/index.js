39d1d17c088278178cd21c6f41b8d2a961146b67
cihattasci <63815355+cihattasci@users.noreply.github.com>
2021-10-15 20:15:15 +0300

    tried to finish upload screen design

--- Screens/UploadPage/index.js
+++ Screens/UploadPage/index.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react'
-import { Text, SafeAreaView } from 'react-native'
+import { Text, SafeAreaView, View, TouchableOpacity } from 'react-native'
+import {styles} from './styles'
 
 export default class index extends Component {
+
+  addPhoto = () => {
+    alert('d')
+  };
+
+  completeSession = () => {
+    alert('d')
+  };
+
   render() {
     return (
-      <SafeAreaView>
-        <Text> textInComponent </Text>
+      <SafeAreaView style={styles.main}>
+        <View style={styles.headerContainer}>
+          <Text style={styles.headerText}>
+            Uploaded Documents
+          </Text>
+        </View>
+        <View style={styles.photos}>
+
+        </View>
+        <View style={styles.addPhotoContainer}>
+          <TouchableOpacity onPress={this.addPhoto}>
+            <View style={styles.addPhoto}>
+              <Text style={styles.photoText}>
+                + Add Photo
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.bottomPartContainer}>
+          <TouchableOpacity onPress={this.completeSession}>
+            <View style={styles.buttonContainer}>
+              <Text style={styles.textContainer}>
+                Complete Session
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
     )
   }
--- Screens/UploadPage/styles.js
+++ Screens/UploadPage/styles.js
@@ -1,5 +1,54 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
+const {width, height} = Dimensions.get("screen");
 
 export const styles = StyleSheet.create({
-    
+    main: {
+        flex: 1,
+    },
+    headerContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    headerText: {
+        color: 'black',
+        fontSize: width*0.05
+    },
+    photos: {
+        flex: 2,
+    },
+    addPhotoContainer: {
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    addPhoto: {
+        width: width*0.8,
+        height: height*0.25,
+        borderWidth: 1,
+        borderColor: 'black',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    photoText: {
+        fontSize: width*0.05,
+        color: 'black',
+    },
+    bottomPartContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonContainer: {
+        width: width*0.35,
+        height: height*0.05,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: 'black'
+    },
+    textContainer: {
+        color: 'black',
+    },
 })
