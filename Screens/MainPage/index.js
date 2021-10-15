3e12fc841775b468779727ce417c5b3eb108f755
cihattasci <63815355+cihattasci@users.noreply.github.com>
2021-10-15 15:41:29 +0300

    created and set navigation screens

---
+++ Screens/MainPage/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, SafeAreaView } from 'react-native'
+
+export default class index extends Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <Text> textInComponent </Text>
+      </SafeAreaView>
+    )
+  }
+}
